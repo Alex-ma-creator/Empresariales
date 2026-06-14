9ad195316fcdb9bb1d2cd402b264e662014200e6
Alex Huayta <alexhuayta2007@gmail.com>
2026-06-14 11:39:04 -0500

    Fix: API URL para produccion

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://127.0.0.1:8000/api' })
+const API = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL 
+    ? `${import.meta.env.VITE_API_URL}/api` 
+    : 'http://127.0.0.1:8000/api' 
+})
 
 API.interceptors.request.use(config => {
   const token = localStorage.getItem('token')
